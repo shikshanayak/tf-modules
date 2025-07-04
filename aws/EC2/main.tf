464a36d0e4085e6350ef4fd84df49b710785607f
shiksha nayak <shikshanayak111@gmail.com>
2025-07-04 19:43:07 +0530

    create ec2 with sg

--- aws/EC2/main.tf
+++ aws/EC2/main.tf
@@ -6,6 +6,13 @@ module "ec2-instance" {
   instance_type = var.instance_type 
   vpc_security_group_ids = var.vpc_security_group_ids 
   subnet_id = var.subnet_id
+  create_security_group = var.create_security_group
+  security_group_description = var.create_security_group
+  security_group_ingress_rules = var.security_group_ingress_rules
+  # security_group_egress_rules = var.security_group_egress_rules
+  security_group_name = var.security_group_name 
+  security_group_vpc_id = var.security_group_vpc_id
+
 #   tags = var.tags
 
 }
--- aws/EC2/variable.tf
+++ aws/EC2/variable.tf
@@ -17,6 +17,35 @@ variable vpc_security_group_ids {
 variable "subnet_id" {
   type = string
 }
+
+variable create_security_group {
+    type = bool
+}
+
+
+variable security_group_description {
+    type = string
+}
+
+variable security_group_ingress_rules {
+    type = map(object({
+        description = optional(string)
+        ip_protocol = optional(string)
+        cidr_ipv4 = optional(string)
+        to_port = optional(number)
+        from_port = optional(number)
+    }))
+    
+}   
+
+variable security_group_name {
+    type = string
+}
+
+variable security_group_vpc_id {
+    type = string
+}
+
 # variable tags {
 #     type = map(string)
 # }
