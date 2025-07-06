92b92f4b433d31043fab2bc1e2a40af85de04e3f
shiksha nayak <shikshanayak111@gmail.com>
2025-07-06 12:08:23 +0530

    adding ec2 and vpc modules

--- aws/EC2/main.tf
+++ aws/EC2/main.tf
@@ -2,9 +2,9 @@ module "ec2-instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "6.0.1"
   ami = var.instance_ami
-  key_name = var.key_name
+  # key_name = var.key_name
   instance_type = var.instance_type 
-  vpc_security_group_ids = var.vpc_security_group_ids 
+  # vpc_security_group_ids = var.vpc_security_group_ids 
   subnet_id = var.subnet_id
   create_security_group = var.create_security_group
   security_group_description = var.create_security_group
@@ -12,7 +12,5 @@ module "ec2-instance" {
   # security_group_egress_rules = var.security_group_egress_rules
   security_group_name = var.security_group_name 
   security_group_vpc_id = var.security_group_vpc_id
-
-#   tags = var.tags
 
 }
--- aws/EC2/variable.tf
+++ aws/EC2/variable.tf
@@ -2,17 +2,17 @@ variable instance_ami {
     type = string
 }
 
-variable key_name {
-    type = string
-}
+# variable key_name {
+#     type = string
+# }
 
 variable instance_type {
     type = string
 }
 
-variable vpc_security_group_ids {
-    type = list(string)
-}
+# variable vpc_security_group_ids {
+#     type = list(string)
+# }
 
 variable "subnet_id" {
   type = string
